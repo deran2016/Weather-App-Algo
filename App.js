8c45a470afa444e2326877dcb40402452c9670a2
Inyeob Kim <deran2016@gmail.com>
2020-03-21 13:03:25 +0900

    add Weather.js component

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './Components/Weather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  state = {
+      isLoaded: true
+  }
+
+  render() {
+      const { isLoaded } = this.state;
+      return (
+          <View style={styles.container}>
+              { isLoaded ? <Weather/>: (
+                  <View style={styles.loading}>
+                      <Text style={styles.loadingText}>Getting the weather</Text>
+                  </View>
+              )}
+          </View>
+      );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  loading:{
+    flex: 1,
+    backgroundColor:'#FDF6AA',
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText:{
+    fontSize: 38,
+    marginBottom: 100
+  }
 });
